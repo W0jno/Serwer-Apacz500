38414c467a32c8a3fbc9c98e29be45ea87850f3a
W0jno <filip.wojno03@gmail.com>
2025-10-14 13:34:56 +0200

    MQTT

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from asyncio_mqtt import Client
+import asyncio
 from datetime import datetime
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
+
+mqqt_client = Client("localhost")
 
 class Device(BaseModel):
     device_id: str
     ip: str
+    status: str
+    modules: List[str]
+
 
 connected_devices = {}
 
@@ -25,3 +33,19 @@ def update_device(device: Device):
 def list_devices():
     return connected_devices
 
+@app.on_event("startup")
+async def start_mqtt():
+    asyncio.create_task(mqtt_listener())
+
+async def mqtt_listener():
+    async with mqtt_client as client:
+        await client.subscribe("devices/#")
+        async with client.unfiltered_messages() as messages:
+            async for msg in messages:
+                print(f"{msg.topic} -> {msg.payload.decode()}")
+
+@app.post("/command/{device_id}")
+async def command(device_id: str, command: str):
+    topic = f"devices/{device_id}/cmd"
+    await mqtt_client.publish(topic, command.encode())
+    return {"sent": command, "to": device_id}
