6c0667055eb8c12184923d014478215e4e4577bb
W0jno <filip.wojno03@gmail.com>
2025-10-20 13:35:16 +0200

    Commands

--- main.py
+++ main.py
@@ -12,6 +12,12 @@ class Device(BaseModel):
     device_id: str
     ip: str
     status: str
+    is_working: bool
+
+class DeviceCommand(BaseModel):
+    src_device_id: str
+    dst_device_id: str
+    cmd: str
 
 # Słownik urządzeń podłączonych, klucz = device_id
 connected_devices: Dict[str, Device] = {}
@@ -48,20 +54,22 @@ async def mqtt_listener():
                         connected_devices[device_id] = Device(
                             device_id=device_id,
                             ip=ip,
-                            status=status
+                            status=status,
+                            is_working=False
                         )
                     print(f"Updated {device_id}: {payload}")
 
-@app.get("devices/chain/")
-async def chain(
-    src_device_id: str = Query(...),
-    dst_device_id: str = Query(...),
-    cmd: str = Query(...)
-):
-    topic = f"/devices/chain/{dst_device_id}"
+@app.post("/devices/cmd")
+async def chain(command: DeviceCommand):
+    topic = f"/devices/{command.dst_device_id}/cmd"
+
     try:
-        await mqtt_client.publish(topic, cmd.encode())
-        return {"status": "ok", "topic": topic, "cmd": cmd}
+        if(connected_devices[command.dst_device_id].is_working == False):
+            await mqtt_client.publish(topic, command.cmd.encode())
+            connected_devices[command.dst_device_id].is_working = True
+            return {"status": "ok", "topic": topic, "cmd": command.cmd, "target": command.dst_device_id}
+        else:
+            return{"status": "error", "detail": "Destination device is busy", "target": command.dst_device_id}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
